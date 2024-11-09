f6358a74633b12a2f5fdbeab1f41b5de7860ef52
manish <manish.parmar@tntra.io>
2024-11-09 12:48:16 +0530

    [update]model file in docker porject

--- my_docker/test_project/test_app/models.py
+++ my_docker/test_project/test_app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 # Create your models here.
 class Test(models.Model):
     name = models.CharField()
+    designation = models.CharField()
+
+    def __str__(self):
+        return f" My Name is {self.name} a {self.designation}"
